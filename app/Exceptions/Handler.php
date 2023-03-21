526cbe77101d4e43fd1f6a41508d494b21d37d0a
Guillermo Enrique <gsanchez1687@gmail.com>
2023-03-21 19:19:49 -0400

    Controlando excepciones de autenticacion

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Exceptions;
+use Exception;
 use App\Traits\ApiResponser;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
@@ -16,7 +17,12 @@ class Handler extends ExceptionHandler
     ];
 
     protected $dontReport = [
-        //
+        \Illuminate\Auth\AuthenticationException::class,
+        \Illuminate\Auth\Access\AuthorizationException::class,
+        \Symfony\Component\HttpKernel\Exception\HttpException::class,
+        \Illuminate\Database\Eloquent\ModelNotFoundException::class,
+        \Illuminate\Session\TokenMismatchException::class,
+        \Illuminate\Validation\ValidationException::class,
     ];
 
     protected $dontFlash = [
@@ -24,6 +30,11 @@ class Handler extends ExceptionHandler
         'password',
         'password_confirmation',
     ];
+
+    public function report(Exception $exception)
+    {
+        parent::report($exception);
+    }
 
   
     public function register(): void
@@ -44,7 +55,13 @@ class Handler extends ExceptionHandler
             $modelo =  strtolower(class_basename($exception->getModel()));
             return $this->errorResponse("No existe Ninguna Instancia del modelo {$modelo} con el id espeficico", 404);
         }
+
         return parent::render($request, $exception);//hacemos uso del render del framework
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return $this->errorResponse('No autenticado.', 401);        
     }
 
     protected function convertValidationExceptionToResponse(ValidationException $e, $request)
