0f0d6d68ed626427f2e45a0e7aaf0aafc9c85db6
Guillermo Enrique <gsanchez1687@gmail.com>
2023-03-22 20:10:53 -0400

    Controlando excepciones de forma general y controlando los Query

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,13 +1,15 @@
 <?php
 
 namespace App\Exceptions;
-use Exception;
 use App\Traits\ApiResponser;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
@@ -37,9 +39,6 @@ class Handler extends ExceptionHandler
         $errors = $e->validator->errors()->getMessages();
         return $this->errorResponse($errors, 422);
     }
-
-  
-
   
     public function register(): void
     {
@@ -65,13 +64,32 @@ class Handler extends ExceptionHandler
         }
 
         if($exception instanceof AuthorizationException ){
-            return $this->errorResponse('No tienes permisos para ejecutar esta accion',403);
+            return $this->errorResponse('No tienes permisos para ejecutar esta acción',403);
         }
 
         //Capturando cuando introducen una url equivocada
         if($exception instanceof NotFoundHttpException ){
-            return $this->errorResponse('No se encontro la ruta',404);
+            return $this->errorResponse("No se encontro la ruta: ".$request->url(),404);
         }
+
+         //Capturando cuando no existe el metodo
+         if($exception instanceof MethodNotAllowedHttpException ){
+            return $this->errorResponse("El método ".$request->method()." especificado en la petición no es válido",405);
+        }
+
+         //Controlando cualquier exception
+         if($exception instanceof HttpException ){
+            return $this->errorResponse($exception->getMessage(),$exception->getStatusCode());
+        }
+
+        //Controlando los Query
+        if($exception instanceof QueryException ){
+            $code = $exception->errorInfo[1];
+            if($code == 1451)
+                return $this->errorResponse("No se puede eliminar porque el ID tiene relaciones con otras tablas",409);
+        }
+
+
 
 
         return parent::render($request, $exception);//hacemos uso del render del framework
