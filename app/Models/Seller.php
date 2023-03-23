8af7722c2ea0c4bfe9e1df794b5ff4a9bff91319
Guillermo Enrique <gsanchez1687@gmail.com>
2023-03-22 22:38:32 -0400

    Implementando Scopes en seller

--- app/Models/Seller.php
+++ app/Models/Seller.php
@@ -2,12 +2,19 @@
 
 namespace App\Models;
 
+use App\Scopes\SellerScopes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Seller extends User
 {
     use HasFactory;
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::addGlobalScope(new SellerScopes);
+    }
 
     public function products(){
         return $this->hasMany(Product::class);
